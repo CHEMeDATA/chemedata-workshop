c864c9199b1c5207452bea189ef3007ceb6f44c8
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-04-09 23:36:49 +0200

    changed index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 export function myModule() {
+  let http = require('http');
+  //let server = http.createServer(function (_request, response) {
+  http.createServer(function (_request, response) {
+    response.writeHead(200, {
+      'Content-Type': 'text/plain',
+    });
+    response.write('This is Test Message from index.js');
+    response.end();
+  });
+  //server.listen(8082);
   return 42;
 }

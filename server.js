8af8361a5e1293c38dd72c71fda901f76bb36b55
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-04-14 21:16:53 +0200

    tried other test

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
-// server.js
-'use strict'
+const fastify = require('fastify')({ logger: true })
 
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
-  })
+fastify.route({
+  method: 'GET',
+  url: '/',
+  schema: {
+    // request needs to have a querystring with a `name` parameter
+    querystring: {
+      name: { type: 'string' }
+    },
+    // the response needs to be an object with an `hello` property of type 'string'
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: { type: 'string' }
+        }
+      }
+    }
+  },
+  // this function is executed for every request before the handler is executed
+  beforeHandler: async (request, reply) => {
+    // E.g. check authentication
+  },
+  handler: async (request, reply) => {
+    return { hello: 'world3' }
+  }
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 }
+start()

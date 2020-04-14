010694db691d895540876cfdb0c44828b6ca3b7b
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-04-14 20:46:56 +0200

    try very simple fastify demo

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
-// example copied from https://www.npmjs.com/package/fastify
-let fs = require('fs');
-let path = require('path');
-let fastify = require('fastify');
+const fastify = require('fastify')()
 
-let http = require('http');
-let url = require('url');
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
 
-let serverfastify = fastify({
-  logger: false,
-});
-
-serverfastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'html'),
-});
-
-serverfastify.get('/html', function (req, reply) {
-  reply.sendFile('page1.html'); // serving path.join(__dirname, 'public', 'myHtml.html') directly
-});
-
-serverfastify.listen(8080, (err, address) => {
-  if (err) throw err;
-  serverfastify.log.info(`server listening on... ${address}`);
-});
+const start = async () => {
+  try {
+    await fastify.listen(8080)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
